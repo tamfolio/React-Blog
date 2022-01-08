230348673b1bf434cc673c6b5923593100d7fbe9
unknown <tamiloreolaniyi@gmail.com>
2022-01-08 12:06:35 +0100

    conditional Loading message

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,21 +4,26 @@ import BlogList from './BlogList'
 
 function Homepage() {
    const [blogs, setBlogs] = useState(null);
+   const [isLoading, setIsLoading] = useState(true);
 
 
    useEffect(() => {
-       fetch('http://localhost:8000/blogs')
-       .then(response => {
-          return response.json();
-       })
-       .then((data) => {
-           console.log(data)
-           setBlogs(data)
-       })
+       setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(response => {
+           return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+            setBlogs(data)
+            setIsLoading(false);
+        })
+       }, 1000 );
        }, []);
 
     return (
         <div className='home'>
+            {isLoading && <div>Loading....</div>}
            { blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
         
