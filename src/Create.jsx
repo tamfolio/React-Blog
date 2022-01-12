2e867255afc63247a41dea5229427f19db0d279b
unknown <tamiloreolaniyi@gmail.com>
2022-01-12 14:23:37 +0100

    Redirecting after Post

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import { useState } from 'react'
+import { useHistory } from 'react-router-dom';
 
 function Create() {
 
     const[title, setTitle] = useState('');
     const[body, setBody] = useState('');
     const[author, setAuthor] = useState('mario');
+    const[isPending, setIsPending] = useState('false');
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added')
+            setIsPending(false);
+            history.push('/');
+        })
+
+        
+    }
     return (
         <div className='create'>
             <h2>Add a new Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title</label>
                 <input 
                 type="text"
@@ -33,8 +54,8 @@ function Create() {
                     <option value="Mario">Mario</option>
                     <option value="Yoshi">Yoshi</option>
                 </select>
-                <button>Add Blog</button>
-                <p>{body}</p>
+                {isPending && <button>Add Blog</button>}
+                {!isPending && <button disabled>Adding Blog...</button>}
             </form>
         </div>
     )
